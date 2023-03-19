d8d8b30c5e51bf67847d7f4a14285c8afa186bb0
Ekaterina Pavluk <rinapalijlinnik@gmail.com>
2023-03-19 19:49:23 +0300

    all

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,138 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47
+// Console.WriteLine("введите количество строк");
+// int lines = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите количество столбцов");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// double[,] numbers = new double[lines, columns];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// void FillArrayRandomNumbers(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = Convert.ToDouble(new Random().Next(50,150));
+//         }
+//     }
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
+
+
+// Задача 50
+
+// Console.WriteLine("введите номер строки");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите номер столбца");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int [,] numbers = new int [3,5];
+// FillArrayRandomNumbers(numbers);
+
+// if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+// {
+//     Console.WriteLine("такого элемента нет");
+// }
+// else
+// {
+//     Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+// }
+
+// PrintArray(numbers);
+
+// void FillArrayRandomNumbers(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         {        
+//             for (int j = 0; j < array.GetLength(1); j++)
+//             {
+//                 array [i,j] = new Random().Next(50,150);
+//             }   
+//         }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }   
+//         Console.Write("]");
+//         Console.WriteLine(""); 
+//     }
+// }
+// {
+    
+// }
+
+
+// Задача 52
+
+// Console.WriteLine("введите количество строк");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите количество столбцов");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// int[,] numbers = new int[n, m];
+// FillArrayRandomNumbers(numbers);
+
+
+// for (int k = 0; k < numbers.GetLength(1); k++)
+// {
+//     double avarage = 0;
+//     for (int i = 0; i < numbers.GetLength(0); i++)
+//     {
+//         avarage = (avarage + numbers[i, k]);
+//     }
+//     avarage = avarage / n;
+//     Console.Write(avarage + "; ");
+// }
+// Console.WriteLine();
+// PrintArray(numbers);
+
+
+
+// void FillArrayRandomNumbers(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int k = 0; k < array.GetLength(1); k++)
+//         {
+//             array[i, k] = new Random().Next(10,50);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int k = 0; k < array.GetLength(1); k++)
+//         {
+//             Console.Write(array[i, k] + " ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
+
+
+
